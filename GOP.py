759f49798e184a304d889e8847a5d623c67bab0a
mhoover734 <mhoover734@phm.k12.in.us>
2026-01-28 11:11:02 -0500

    Update GOP.py

--- GOP.py
+++ GOP.py
@@ -1,6 +1,5 @@
 import random
 points = {"user": 0, "bot": 0}
-running = True
 total_turns = 1
 stall = True
 
@@ -8,9 +7,9 @@ def win_check(temp, turn):
     global points, total_turns, stall
     if points[turn] + temp >= 100:
         if turn == "user":
-            print(f"The {turn} won!\nThe score was {points["user"]+temp} to {points["bot"]}.\nStarting new game...\n")
+            print(f"\nThe {turn} won!\nThe score was {points["user"]+temp} to {points["bot"]}.\nStarting new game...\n")
         else:
-            print(f"The {turn} won!\nThe score was {points["user"]} to {points["bot"]+temp}.\nStarting new game...\n")
+            print(f"\nThe {turn} won!\nThe score was {points["user"]} to {points["bot"]+temp}.\nStarting new game...\n")
         points["user"] = 0
         points["bot"] = 0
         total_turns = 0
@@ -23,7 +22,7 @@ def game_turn(turn):
     end_turn = False
     turn_number = 1
     potential_points = 0
-    print(f"\nIt's the {turn}'s turn!")
+    print(f"\nIt's the {turn}'s turn!\n")
     print(f"The score is currently {points["user"]} to {points["bot"]}.")
     while not(end_turn):
         if win_check(potential_points, turn):
@@ -32,20 +31,22 @@ def game_turn(turn):
             print(f"The {turn} has {potential_points} unbanked points, making a potential score of {points[turn]+potential_points}.")
         if turn == "user":
             if turn_number == 1:
-                move = "hold"
+                move = "roll"
             else:
                 while True:
-                    user_input = input("Enter a move (\"Hold\" or \"Bank\"): ").lower()
-                    if user_input not in ("hold", "bank"):
+                    user_input = input("Enter a move -- \"(R)oll\" or \"(B)ank\": ").lower()
+                    if user_input not in ("roll", "bank", "r", "b", "quit"):
                         print("Unrecognized input, try again.")
+                    elif user_input == "quit":
+                        return "quit"
                     else:
                         move = user_input
                         break
         elif potential_points > 18:
             move = "bank"
         else:
-            move = "hold"
-        if move == "hold":
+            move = "roll"
+        if move in ("roll", "r"):
             roll = random.randint(1, 6)
             print(f"The {turn} rolled a {roll}.")
             if roll == 1:
@@ -59,7 +60,11 @@ def game_turn(turn):
             points[turn] += potential_points
             end_turn = True
         turn_number += 1
-    input("Press Enter to continue.")
+        if not(end_turn) and turn == "bot":
+            if input("Press Enter to continue. ").lower() == "quit":
+                return "quit"
+    if input("Press Enter to continue. ").lower() == "quit":
+        return "quit"
 
 def total_turn_calc():
     global total_turns, stall
@@ -70,9 +75,12 @@ def total_turn_calc():
     else:
         stall = False
 
-print(f"\n{"-"*20} Turn {total_turns} {"-"*20}")
-while running:
-    game_turn("user")
+print(f"\nAt any time, enter \"quit\" to quit.\n\n{"-"*20} Turn {total_turns} {"-"*20}")
+while True:
+    if game_turn("user") == "quit":
+        break
     total_turn_calc()
-    game_turn("bot")
+    if game_turn("bot") == "quit":
+        break
     total_turn_calc()
+print("\nQuitting...\n")
